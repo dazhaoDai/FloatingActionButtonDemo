fa2837f783e019bb71ca0ff33710998050104904
dazhaoDai <937083575@qq.com>
2017-05-04 15:35:18 +0800

    修复Bug

--- src/main/java/com/ddz/floagintactionbuttondemo/MainActivity.java
+++ src/main/java/com/ddz/floagintactionbuttondemo/MainActivity.java
@@ -14,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
     @Override
